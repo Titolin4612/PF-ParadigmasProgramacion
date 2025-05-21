99f1651899a1a44f40360aa9d9f3bc2c2f44982e
Chanda <sandano4612@gmail.com>
2025-05-20 19:31:17 -0500

    Juego V 1.44 - Baraja terminada y funcional

--- MVC Web App/obj/Debug/net8.0/MVC_ProyectoFinalPOO.AssemblyInfo.cs
+++ MVC Web App/obj/Debug/net8.0/MVC_ProyectoFinalPOO.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("MVC_ProyectoFinalPOO")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+01ca0b42fb024875c41fafe52a9f8f6c85cf39c1")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+89ed97ab4cc4778de6ef044b5f20caf7fc09ec83")]
 [assembly: System.Reflection.AssemblyProductAttribute("MVC_ProyectoFinalPOO")]
 [assembly: System.Reflection.AssemblyTitleAttribute("MVC_ProyectoFinalPOO")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
